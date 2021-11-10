0ec9db4ab78428859cb33e66e750c94fb6aca54a
HimeYuyuko <69159983+HimeYuyuko@users.noreply.github.com>
2021-11-10 17:41:18 +0900

    Delete bicycle-spring-main directory

--- bicycle-spring-main/src/main/java/bicycle/repository/BicycleRepository.java
+++
@@ -1,9 +0,0 @@
-package bicycle.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import bicycle.entity.Bicycle;
-
-public interface BicycleRepository extends CrudRepository<Bicycle, String>{
-	public Long countBySerialStartsWith(String prefix);
-}
--- bicycle-spring-main/src/main/java/bicycle/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package bicycle.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import bicycle.entity.Member;
-
-public interface MemberRepository extends CrudRepository<Member, String> {}
